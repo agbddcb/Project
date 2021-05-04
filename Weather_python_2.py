0bca580811ea2f85cb729f8c36f4ad97f016be11
agbddcb <82065016+agbddcb@users.noreply.github.com>
2021-05-04 11:18:11 -0700

    Add files via upload

---
+++ Weather_python_2.py
@@ -0,0 +1,11 @@
+import requests
+
+city=input("enter Your City -->")
+Api_key = "a684abc9c4e1166da08989cfdadb417a" # Paste Your API ID Here
+
+final_URL ="http://api.openweathermap.org/data/2.5/weather?q={}&appid={}".format(city,Api_key)
+
+result = requests.get(final_URL)
+data = result.json()
+
+print(data)
